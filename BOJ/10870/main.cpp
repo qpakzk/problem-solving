f388f7bb73c95956359bc25a75d659e00d9f2dad
Sangwon Hong <qpakzk@gmail.com>
2021-07-31 08:30:46 +0900

    boj: solved problem no. 10870

---
+++ BOJ/10870/main.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int f[21];
+    f[0] = 0;
+    f[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        f[i] = f[i - 1] + f[i - 2];
+    }
+
+    printf("%d\n", f[n]);
+    return 0;
+}
