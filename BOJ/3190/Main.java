716706fa7424b62aec327abeeeddbc0950c9eb51
Sangwon Hong <qpakzk@gmail.com>
2021-08-05 00:40:24 +0900

    boj: simplify my solution for problem no. 3190

--- BOJ/3190/Main.java
+++ BOJ/3190/Main.java
@@ -4,107 +4,73 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    private static final int SNAKE = -1;
+    static final int SNAKE = -1;
 
-    private static final int BLANK = 0;
+    static final int BLANK = 0;
 
-    private static final int APPLE = 1;
+    static final int APPLE = 1;
 
-    public static boolean isOver(int x, int y, int n, int[][] board) {
-        return x < 1 || x > n || y < 1 || y > n || board[x][y] == SNAKE;
-    }
+    static int N;
 
-    public static void rotate(int d[], char direction) {
-        int dx = d[0];
-        int dy = d[1];
+    static int K;
 
-        if (dx == 0 && dy == 1) {
-            if (direction == 'D') {
-                d[0] = 1;
-                d[1] = 0;
-            } else {
-                d[0] = -1;
-                d[1] = 0;
-            }
-        } else if (dx == 1 && dy == 0) {
-            if (direction == 'D') {
-                d[0] = 0;
-                d[1] = -1;
-            } else {
-                d[0] = 0;
-                d[1] = 1;
-            }
-        } else if (dx == 0 && dy == -1) {
-            if (direction == 'D') {
-                d[0] = -1;
-                d[1] = 0;
-            } else {
-                d[0] = 1;
-                d[1] = 0;
-            }
-        } else {
-            if (direction == 'D') {
-                d[0] = 0;
-                d[1] = 1;
-            } else {
-                d[0] = 0;
-                d[1] = -1;
-            }
-        }
-    }
+    static int L;
+
+    static int[][] map = new int[101][101];
+
+    static char[] rotate = new char[10000];
+
     public static void main(String[] args) throws IOException {
-        int[][] board = new int[101][101];
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int k = Integer.parseInt(br.readLine());
 
+        N = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < k; i++) {
-            String input = br.readLine();
-            StringTokenizer st = new StringTokenizer(input, " ");
-            int row = Integer.parseInt(st.nextToken());
-            int col = Integer.parseInt(st.nextToken());
-            board[row][col] = APPLE;
+        for (int i = 0; i < K; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            map[x][y] = APPLE;
         }
 
-        int l = Integer.parseInt(br.readLine());
-        char[] rotate = new char[10000];
-        for (int i = 0; i < l; i++) {
-            String input = br.readLine();
-            StringTokenizer st = new StringTokenizer(input, " ");
+        L = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < L; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int t = Integer.parseInt(st.nextToken());
             char d = st.nextToken().charAt(0);
             rotate[t] = d;
         }
 
-        board[1][1] = SNAKE;
-
-        int[] d = {0, 1};
-        LinkedList<Coordinate> snake = new LinkedList<>();
+        map[1][1] = SNAKE;
+        Queue<Coordinate> snake = new LinkedList<>();
         snake.add(new Coordinate(1, 1));
 
+        int[] d = {0, 1};
+
+        int headX = 1, headY = 1;
         int time = 0;
-        int headX = 1;
-        int headY = 1;
         while (true) {
             time++;
 
             headX += d[0];
             headY += d[1];
 
-            if (isOver(headX, headY, n, board)) break;
+            if (isOver(headX, headY)) break;
 
             snake.add(new Coordinate(headX, headY));
-            if (board[headX][headY] != APPLE) {
-                Coordinate coordinate = snake.removeFirst();
-                board[coordinate.getX()][coordinate.getY()] = BLANK;
+
+            if (map[headX][headY] != APPLE) {
+                Coordinate remove = snake.remove();
+                map[remove.x][remove.y] = BLANK;
             }
-            board[headX][headY] = SNAKE;
+
+            map[headX][headY] = SNAKE;
 
             if (rotate[time] != 0) {
                 rotate(d, rotate[time]);
@@ -114,20 +80,48 @@ public class Main {
         System.out.println(time);
     }
 
-    static class Coordinate {
-        private final int x, y;
+    static boolean isOver(int x, int y) {
+        return x < 1 || x > N || y < 1 || y > N ||
+                map[x][y] == SNAKE;
+    }
 
-        public Coordinate(int x, int y) {
+    static void rotate(int[] d, char rotate) {
+        int dx = d[0], dy = d[1];
+
+        if (dx == 0 && dy == 1) {
+            if (rotate == 'D') {
+                d[0] = 1; d[1] = 0;
+            } else {
+                d[0] = -1; d[1] = 0;
+            }
+        } else if (dx == 1 && dy == 0) {
+            if (rotate == 'D') {
+                d[0] = 0; d[1] = -1;
+            } else {
+                d[0] = 0; d[1] = 1;
+            }
+        } else if (dx == 0 && dy == -1) {
+            if (rotate == 'D') {
+                d[0] = -1; d[1] = 0;
+            } else {
+                d[0] = 1; d[1] = 0;
+            }
+        } else {
+            if (rotate == 'D') {
+                d[0] = 0; d[1] = 1;
+            } else {
+                d[0] = 0; d[1] = -1;
+            }
+        }
+    }
+
+    static class Coordinate {
+        int x;
+        int y;
+
+        Coordinate(int x, int y) {
             this.x = x;
             this.y = y;
-        }
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
         }
     }
 }
