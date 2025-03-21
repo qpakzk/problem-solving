7d084b255d097667b71a076894a5a302b33d4cab
Sangwon Hong <qpakzk@gmail.com>
2025-03-22 00:51:38 +0900

    Title: 외주 수익 최대화하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250322/외주 수익 최대화하기/max-of-outsourcing-profit.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader()
+    }
+
+    static class Work {
+        int t;
+        int p;
+    }
+}
