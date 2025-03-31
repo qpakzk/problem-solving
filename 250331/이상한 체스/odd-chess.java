0860e947e93ce05fc6bea9952db6e8f2737164bb
Sangwon Hong <qpakzk@gmail.com>
2025-03-31 20:10:50 +0900

    Title: 이상한 체스, Time: 132ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250331/이상한 체스/odd-chess.java
+++ 250331/이상한 체스/odd-chess.java
@@ -2,18 +2,28 @@ import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
 
+class Pair {
+	int x, y;
+	
+	public Pair(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
 public class Main {
-	public static final int MAX_N = 8, MAX_M = 8;
+	public static final int INT_MAX = Integer.MAX_VALUE;
+	public static final int MAX_N = 8;
+	public static final int MAX_M = 8;
 	
 	public static int n, m;
+	public static int minArea = INT_MAX;
 	public static int[][] board = new int[MAX_N][MAX_M];
+	public static List<Pair> chessPieces = new ArrayList<>();
+	public static int[][] pieceDir = new int[MAX_N][MAX_M];
 	public static boolean[][] visited = new boolean[MAX_N][MAX_M];
-	public static int[][] horseDir = new int[MAX_N][MAX_M];
-	public static List<Pair> horses = new ArrayList<>();
 	
-	public static int minEmptyGrid = Integer.MAX_VALUE;
-	
-	public static final int[][] canMove = new int[][] {
+	public static int[][] canMove = new int[][] {
 		{},
 		{1, 0, 0, 0},
 		{0, 1, 0, 1},
@@ -22,72 +32,78 @@ public class Main {
 		{1, 1, 1, 1}
 	};
 	
-	public static void fill(int curX, int curY) {		
-		int horseNum = board[curX][curY];
-		int curDir = horseDir[curX][curY];
-		
-		int[] dx = {-1, 0, 1, 0};
-		int[] dy = {0, 1, 0, -1};
+	public static boolean inRange(int x, int y) {
+		return 0 <= x && x < n && 0 <= y && y < m;
+	}
+	
+	public static boolean canGo(int x, int y) {
+		return inRange(x, y) && board[x][y] != 6;
+	}
+	
+	public static void fill(int startX, int startY, int pieceNum, int faceDir) {
+		int[] dx = new int[]{-1, 0, 1, 0};
+		int[] dy = new int[]{0, 1, 0, -1};
 		
 		for (int i = 0; i < 4; i++) {
-			if (canMove[horseNum][i] == 0) {
+			if (canMove[pieceNum][i] == 0) {
 				continue;
 			}
 			
-			int x = curX, y = curY;
-			int moveDir = (i + curDir) % 4;
+			int x = startX, y = startY;
+			int moveDir = (i + faceDir) % 4;
 			
 			while (true) {
 				visited[x][y] = true;
-				int nx = x + dx[moveDir];
-				int ny = y + dy[moveDir];
-				
-				if (0 > nx || nx > n || 0 > ny || ny > m 
-						|| board[nx][ny] == 6) {
+				int nx = x + dx[moveDir], ny = y + dy[moveDir];
+				if (canGo(nx, ny)) {
+					x = nx;
+					y = ny;
+				}
+				else {
 					break;
 				}
-				
-				x = nx;
-				y = ny;
 			}
 		}
 	}
 	
-	public static int getEmptyGrid() {
+	public static int getArea() {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				visited[i][j] = false;
 			}
 		}
 		
-		for (Pair horse : horses) {
-			fill(horse.x, horse.y);
+		for (int i = 0; i < chessPieces.size(); i++) {
+			int x = chessPieces.get(i).x;
+			int y = chessPieces.get(i).y;
+			
+			fill(x, y, board[x][y], pieceDir[x][y]);
 		}
 		
-		int emptyGrid = 0;
+		int area = 0;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				if (!visited[i][j] && board[i][j] != 6) {
-					emptyGrid += 1;
+					area++;
 				}
 			}
 		}
 		
-		return emptyGrid;
+		return area;
 	}
 	
-	public static void searchMinEmptyGrid(int count) {
-		if (count == horses.size()) {
-			minEmptyGrid = Math.min(minEmptyGrid, getEmptyGrid());
+	public static void searchMinArea(int cnt) {
+		if (cnt == chessPieces.size()) {
+			minArea = Math.min(minArea, getArea());
 			return;
 		}
 		
-		int x = horses.get(count).x;
-		int y = horses.get(count).y;
+		int pieceX = chessPieces.get(cnt).x;
+		int pieceY = chessPieces.get(cnt).y;
 		
 		for (int i = 0; i < 4; i++) {
-			horseDir[x][y] = i;
-			searchMinEmptyGrid(count + 1);
+			pieceDir[pieceX][pieceY] = i;
+			searchMinArea(cnt + 1);
 		}
 	}
 	
@@ -100,23 +116,13 @@ public class Main {
 			for (int j = 0; j < m; j++) {
 				board[i][j] = sc.nextInt();
 				if (1 <= board[i][j] && board[i][j] <= 5) {
-					horses.add(new Pair(i, j));
+					chessPieces.add(new Pair(i, j));
 				}
 			}
 			
-			searchMinEmptyGrid(0);
+			searchMinArea(0);
 		}
+		System.out.println(minArea);
 		
-		System.out.println(minEmptyGrid);
-		sc.close();
-	}
-	
-	static class Pair {
-		int x, y;
-		
-		public Pair(int row, int col) {
-			this.x = row;
-			this.y = col;
-		}
 	}
 }
